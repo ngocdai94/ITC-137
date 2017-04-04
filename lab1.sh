ed0360b8fc71c14695eaa2b9bed8292a66dbbb7c
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-04-04 14:47:28 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,7 @@
 # This is a comment
+
+pwd              # Stands for 'Print Working Directory' and tells me where I am (WHERE)
+whoami           # Tells me which user I'm logged in as (WHO)
+ls               # Tells me what files are in the directory (WHAT)
+cd               # Allows me to change into other direcotries (NAVIGATION)
+date             # What time is it on the machine (WHEN)
